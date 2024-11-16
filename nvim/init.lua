ed54d046e3d51c15002e9d46f717afda90779e91
Umxn2 <umanggoel123456@gmail.com>
2024-11-17 05:23:39 +0530

    latest

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,4 +10,10 @@ for _, group in ipairs(highlight_groups) do
   vim.api.nvim_set_hl(0, group, { bg = "NONE" })
 end
 require("config.lazy")
+vim.g.terminal_emulator = "wezterm"
+vim.opt.termguicolors = true
 vim.cmd("highlight NoiceCmdlinePopupBorder guifg=#b4dced")
+vim.cmd([[
+  highlight TermNormal guibg=NONE ctermbg=NONE
+  highlight TermFloat guibg=NONE ctermbg=NONE
+]])
