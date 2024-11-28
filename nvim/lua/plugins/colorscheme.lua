a2f4375f147026365fd7fc3bdb2d6716fcd55dcb
Umxn2 <umanggoel123456@gmail.com>
2024-11-28 06:44:31 +0530

    Updated color scheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -15,9 +15,9 @@ return {
       no_italic = true,
       color_overrides = {
         mocha = {
-          base = "#1a1a1a",
-          mantle = "#1a1a1a",
-          crust = "#1a1a1a",
+          base = "#191919",
+          mantle = "#191919",
+          crust = "#191919",
           lavender = "#cdeaf7", -- cloudy blue
           mauve = "#6fc9e7", -- saffire
           blue = "#fba4d2", -- pink
@@ -25,7 +25,7 @@ return {
           green = "#c0c0c0", -- silver
           red = "#ff7bb4", -- dark pink
           maroon = "#fecca8", -- peach
-          yellow = "#fde5b4", -- light yellow
+          yellow = "#fde5b4",
           text = "#eeeeee",
           surface0 = "#202020",
         },
